bd4cf87344d31e6e041a6201337077633ee19982
GiuliaLanzillotta <lanzillottagiulia97@gmail.com>
2021-04-06 12:28:39 +0200

    Conv layers

--- models/VAE.py
+++ models/VAE.py
@@ -1,2 +1,49 @@
 # Code for beta-VAE
 # Paper: https://openreview.net/forum?id=Sy2fzU9gl 
+import numpy as np
+from torch import nn
+
+class VAE(nn.Module):
+
+    def __init__(self, dim_in, latent_dim, channels_list:List=None) -> None:
+        super(VAE, self).__init__()
+        C, H, W = dim_in
+        self.latent_dim = latent_dim
+        if channels_list is None:
+            channels_list = [32, 64, 128, 256, 512]
+        # Building encoder 
+        modules = []
+        for c in channels_list:
+            modules.append(
+                # Conv block - 
+                #TODO: create a separate layer for this
+                nn.Sequential(
+                    nn.Conv2d(C, out_channels=c,kernel_size= 2, stride= 2, padding  = 1),
+                    nn.BatchNorm2d(c),
+                    nn.LeakyReLU())
+            )
+            final_channels = c
+        self.encoder = nn.Sequential(*modules)
+        # 
+        self.fc_mu = nn.Linear(channels_list[-1]*4, latent_dim)
+        self.fc_var = nn.Linear(channels_list[-1]*4, latent_dim)
+
+    def encode(self, input: Tensor) -> List[Tensor]:
+        raise NotImplementedError
+
+    def decode(self, input: Tensor) -> Any:
+        raise NotImplementedError
+
+    def sample(self, batch_size:int, current_device: int, **kwargs) -> Tensor:
+        raise RuntimeWarning()
+
+    def generate(self, x: Tensor, **kwargs) -> Tensor:
+        raise NotImplementedError
+
+    @abstractmethod
+    def forward(self, *inputs: Tensor) -> Tensor:
+        pass
+
+    @abstractmethod
+    def loss_function(self, *inputs: Any, **kwargs) -> Tensor:
+        pass
--- models/layers.py
+++ models/layers.py
@@ -1,1 +1,52 @@
 # Implementation of useful layers 
+from torch import nn
+from torch import Tensor
+import numpy as np
+
+
+class ConvBlock(nn.Module):
+    """ Implements logic for a convolutional block [conv2d, normalization, activation]"""
+
+    def __init__(self, C_IN, C_OUT, k, s, p):
+        #TODO: add check for the norm
+        #TODO: add selector for activation 
+        super(ConvBlock, self).__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(C_IN, out_channels=C_OUT, kernel_size=k, stride=s, padding=p),
+            nn.BatchNorm2d(C_OUT),nn.LeakyReLU())
+
+    def forward(self, inputs: Tensor) -> Tensor:
+        outputs = self.block(inputs)
+        return outputs
+
+
+class ConvNet(nn.Module):
+    """ Implements convolutional net with multiple convolutional 
+    blocks  + final flattening """
+
+    def __init__(self, dim_in, final_dim, channels_list:list=None):
+        #TODO add selection for non-linearity
+        super(ConvNet, self).__init__()
+        C, H, W = dim_in 
+        if channels_list is None: channels_list = [32, 64, 128, 256]
+        # calculating shape of the image after convolution 
+        final_shape = channels_list[-1], H // 2**(len(channels_list)), W // 2**(len(channels_list))
+        print(final_shape)
+        assert all(v>0 for v in final_shape), "Input not big enough for the convolutions requested"
+        flat_dim = int(np.product(final_shape))
+        
+        # Stacking the conv layers  
+        modules = []
+        for c in channels_list:
+            # conv block with kernel size 2, size 2 and padding 1 
+            # halving the input dimension at every step 
+            modules.append(ConvBlock(C, c, 2, 2, 0)) 
+            C = c
+        modules.append(nn.Flatten())
+        modules.append(nn.Linear(flat_dim, final_dim)) #notice no activation here
+        self.net = nn.Sequential(*modules)
+
+
+    def forward(self, inputs: Tensor) -> Tensor: 
+        outputs = self.net(inputs)
+        return outputs
