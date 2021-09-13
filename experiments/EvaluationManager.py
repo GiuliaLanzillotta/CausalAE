causal plots (1), little fixes

experiments/EvaluationManager.py
@@ -11,7 +11,9 @@ import os
 import glob
 import time
 import copy
-from visualisations import ModelVisualiser, SynthVecDataVisualiser, vis_responses
+
+from experiments.utils import temperature_exponential_annealing
+from visualisations import ModelVisualiser, SynthVecDataVisualiser, vis_responses, vis_xnets
 from metrics import FIDScorer, ModelDisentanglementEvaluator, LatentOrthogonalityEvaluator, LatentConsistencyEvaluator
 import pytorch_lightning as pl
 from metrics.responses import compute_response_matrix
@@ -38,6 +40,8 @@ class ModelHandler(object):
         self._consistencyScorer = None
         self.device = next(self.model.parameters()).device
         self.send_model_to(self.device)
+        # update Xnet tau
+        if "X" in self.model_name: self.reset_tau()
 
     @classmethod
     def from_experiment(cls, experiment:pl.LightningModule, **kwargs):
@@ -49,6 +53,12 @@ class ModelHandler(object):
                                  version=model_version, data_version=data_version)
         experiment = pick_model_manager(model_name)(params = config, verbose=kwargs.get("verbose"))
         return cls(experiment, **kwargs)
+
+    def reset_tau(self, step_num=None):
+        if step_num is None: step_num = self.experiment.global_step
+        tau = temperature_exponential_annealing(step_num)
+        self.model.tau = tau
+        print(f"Tau set to {tau}")
 
     def score_disentanglement(self, **kwargs):
         """Scoring model on disentanglement metrics"""
@@ -245,8 +255,7 @@ class ModelHandler(object):
             print(f"Intervening on {u} ...")
             with torch.no_grad():
                 # interventions on unit u
-                errors = self._consistencyScorer.noise_equivariance(unit=u, unit_dim=U, num_units=D,
-                                                                    device=self.device, **kwargs)
+                errors = self._consistencyScorer.noise_equivariance(unit=u, xunit_dim=U, device=self.device, **kwargs)
                 equivariances[u,:] = errors
 
         equivariances = 1.0 - equivariances
@@ -377,13 +386,13 @@ class ModelHandler(object):
             scores["FID_gen"] = self.score_FID(generation=True, **kwargs)
         if invariance:
             invariances, _ = self.evaluate_invariance(**kwargs)
-            scores["INV"] = torch.sum(invariances*(1-torch.eye(6, device=self.device))).cpu() #TODO: correct to take into account ignored dimensions (maybe divide by posterior sd?)
+            scores["INV"] = torch.sum(invariances*(1-torch.eye(self.model.latent_size, device=self.device))).cpu().numpy() #TODO: correct to take into account ignored dimensions (maybe divide by posterior sd?)
         if equivariance:
             equivariances = self.evaluate_equivariance(**kwargs)
-            scores["EQV"] = torch.sum(equivariances).cpu()
+            scores["EQV"] = torch.sum(equivariances).cpu().numpy()
         if self_consistency:
             consistencies, _ = self.evaluate_self_consistency(**kwargs)
-            scores["SCN"] = torch.sum(consistencies).cpu()
+            scores["SCN"] = torch.sum(consistencies).cpu().numpy()
         end = time.time()
 
         print("Time elapsed for scoring {:.0f}".format(end-start))
@@ -434,7 +443,8 @@ class VisualModelHandler(ModelHandler):
                    do_random_samples=False, do_traversals=False, do_hybrisation=False,
                    do_loss2distortion=False, do_marginal=False, do_loss2marginal=False,
                    do_invariance=False, do_latent_block=False, do_traversal_responses=False,
-                   do_latent_response_field=False, do_equivariance=False, **kwargs):
+                   do_latent_response_field=False, do_equivariance=False,
+                   do_jointXmarginals=False, **kwargs):
 
         plots = {}
         if self.visualiser is None:
@@ -469,13 +479,13 @@ class VisualModelHandler(ModelHandler):
                                                                   threshold=0., **kwargs)
 
         if do_traversal_responses:
-            all_traversal_latents, all_traversals_responses, prior_samples = \
+            all_traversal_latents, all_traversals_responses, traversals_steps = \
                 vis_responses.traversal_responses(self.model, self.device, **kwargs)
             plots["trvs_responses"] = []
             D = self.model.latent_size
             for d in range(D):
                 fig = self.visualiser.plot_traversal_responses(d, all_traversal_latents[d], all_traversals_responses[d],
-                                                               prior_samples=prior_samples, **kwargs)
+                                                               traversals_steps=traversals_steps, **kwargs)
                 plots["trvs_responses"].append(fig)
 
 
@@ -492,6 +502,15 @@ class VisualModelHandler(ModelHandler):
             response_field, hybrid_grid = vis_responses.response_field(i, j, self.model, self.device, **kwargs)
             X, Y = hybrid_grid
             plots["latent_response_field"] = self.visualiser.plot_vector_field(response_field, X, Y, i=i, j=j, **kwargs)
+
+        if do_jointXmarginals:
+            i = kwargs.pop("i",0)
+            j = kwargs.pop("j", 1)
+            print(f"Plotting X{i}-X{j} joint marginal")
+            Xij, hue = vis_xnets.compute_joint_ij(i, j, self.model, self.device, **kwargs)
+            Xij = Xij.detach().cpu().numpy()
+            plots["Xij"] = self.visualiser.scatterplot_with_line(Xij[:,0], Xij[:,1], hue,
+                                                                 x_name=f"X{j}", y_name=f"X{i}", **kwargs)
 
         return plots
 

visualisations/utils.py
@@ -6,13 +6,16 @@ import numpy as np
 
 from models import GenerativeAE
 
-def get_traversals_steps(steps:int, ranges:List):
+def get_traversals_steps(steps:int, ranges:List, relative=False):
     """Computes the values of the steps for traversal along the selected dimensions in the given ranges.
     @ranges: list of pairs (min, max) identifying the range to cover in each dimension.
-    Note: returns a torch Tensor
-    #TODO: adapt for multi-dim units (need to sample from the prior/posterior directly)
-    """
-    values = torch.vstack([torch.linspace(m,M, steps) for (m,M) in ranges])
+    @relative:bool - whether to return the change to apply to the vector or directly the value to be adopted
+
+    Note: returns a torch Tensor"""
+    if not relative:  values = torch.vstack([torch.linspace(m,M, steps) for (m,M) in ranges])
+    else:
+        #move half of the total range to the right and hald of the total range to the left
+        values = torch.vstack([torch.linspace(-abs(M-m)/2, abs(M-m)/2, steps) for (m,M) in ranges]) # D x N
     return values
 
 
@@ -40,22 +43,24 @@ def do_latent_traversals_multi_dim(model, latent_vector, dimensions, values, dev
         traversals.append(images)
     return torch.cat(traversals, dim=0)
 
-def do_latent_traversals_multi_vec(latent_vectors:torch.Tensor, unit_dim:int, unit:int, values:torch.Tensor, device:str):
+def do_latent_traversals_multi_vec(latent_vectors:torch.Tensor, unit_dim:int, unit:int, values:torch.Tensor, device:str, relative=False):
     """
     Takes a pool of latent vectors and applies multiple hard interventions to the unit 'unit'
     to traverse the latent space.
     The values to be used to intervene on the vectors are given by the 'values' parameter.
     - @latent_vectors: Tensor w/ shape (num samples, num dimensions)
     - @values: shape (num interventions, unit dimension)
+    - @relative:bool - whether the given traversal values are to be considered relative to the initial position or absolute
     #TODO: include multi-dimensional units traversals
 
     Returns a Tensor of size (steps x N x D)
     """
     N, D = latent_vectors.shape
-    steps = values.shape[1]
+    steps = values.shape[0]
     values_expanded = values.view(steps,1).repeat(1, N).to(device)
     traversals_latents = torch.tile(latent_vectors, (steps, 1, 1)).to(device) # final shape = steps x N x D
-    traversals_latents[:,:,unit] = values_expanded
+    if not relative: traversals_latents[:,:,unit] = values_expanded
+    else: traversals_latents[:,:,unit] += values_expanded
     return traversals_latents
 
 

visualisations/vis_responses.py
@@ -6,7 +6,7 @@ from . import utils
 
 
 def traversal_responses(model:GenerativeAE, device, **kwargs):
-    """Plots the amount of distortion recorded on each latent dimension while traversing a single dimension
+    """Computes the amount of distortion recorded on each latent dimension while traversing a single dimension
     kwargs accepted keywords:
         - num_samples: number of samples from the latent space to use in the computation
         - steps: number of steps to take in the traversal
@@ -18,6 +18,7 @@ def traversal_responses(model:GenerativeAE, device, **kwargs):
     if not kwargs.get('num_samples'):
         kwargs['num_samples'] = 50
     steps = kwargs.get('steps',20)
+    relative = kwargs.get('relative',False)
     unit_dim = 1
 
     all_traversal_latents = []
@@ -26,14 +27,14 @@ def traversal_responses(model:GenerativeAE, device, **kwargs):
     prior_samples = model.sample_noise_from_prior(device=device, **kwargs).detach()
     ranges = model.get_prior_range()
     # for each latent unit we start traversal
+    # 1. obtain traversals values
+    traversals_steps = utils.get_traversals_steps(steps, ranges, relative=relative).to(device).detach() #torch Tensor
     for d in range(model.latent_size):
-        range_d = ranges[d]
-        # 1. obtain traversals values
-        traversals_steps = utils.get_traversals_steps(steps, [range_d]).to(device).detach() #torch Tensor
         with torch.no_grad():
             # 2. do traversals
-            traversals = utils.do_latent_traversals_multi_vec(prior_samples, unit_dim=unit_dim, unit=d,
-                                                              values=traversals_steps, device=device) # shape steps x N x D
+            traversals = utils.do_latent_traversals_multi_vec(prior_samples, unit_dim=unit_dim,
+                                                              unit=d, values=traversals_steps[d],
+                                                              device=device, relative=relative) # shape steps x N x D
             traversals_latents = traversals.view(-1, model.latent_size) # reshaping to fit into batch
             # 3. obtain responses
             trvs_response = model.encode_mu(model.decode(traversals_latents, activate=True))
@@ -42,7 +43,7 @@ def traversal_responses(model:GenerativeAE, device, **kwargs):
 
     print("...done")
 
-    return all_traversal_latents, all_traversals_responses, prior_samples
+    return all_traversal_latents, all_traversals_responses, traversals_steps
 
 def hybrid_responses():
     #TODO

visualisations/visualise_model.py
@@ -1,4 +1,6 @@
 """ Script implementing logic for model visualisations """
+from typing import List
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -332,10 +334,11 @@ class ModelVisualiser(object):
         return torch.vstack(losses) # D x n_steps
 
     @staticmethod
-    def plot_traversal_responses(dim:int, latents:Tensor, responses:Tensor, prior_samples:Tensor, **kwargs):
+    def plot_traversal_responses(dim:int, latents:Tensor, responses:Tensor, traversals_steps:Tensor, **kwargs):
         """For the selected latent dimension plots the recorded errors on the responses across the traversal
         @latents: Tensor of shape (steps, N, D)
         @responses:   //        //
+        @traversals_steps: tensor of shape (D, S) with the traversal steps taken for each dimension
         kwargs accepted keys:
         - figure parameters
         - population: bool - whether to plot the whole population trend or the individual samples (default False)
@@ -363,21 +366,16 @@ class ModelVisualiser(object):
             for col in range(ncols):
                 if i==D: break
                 ax[row,col].set_title(f"Responses on latent dimension {i}")
-                if relative:
-                    x = latents[:,:,dim] - prior_samples[:,dim].view(1, -1)
-                    ticks = latents[:,0,dim] - prior_samples[:,dim].mean()
-                else:
-                    x = latents[:,:,dim] # absolute position
-                    ticks = latents[:,0,dim]
+                x = traversals_steps[dim].view(S, 1).repeat(1, N)
                 x = x.view(-1,).cpu().numpy() # steps x N
-                y = (latents[:,:,i] - responses[:,:,i]).view(-1,).cpu().numpy() # steps x N
+                y = (responses[:,:,i] - latents[:,:,i]).view(-1,).cpu().numpy() # steps x N
                 if normalise: y /= torch.std(responses[0,:,i]).cpu().numpy()
                 hue = None if population else [i for _ in range(S) for i in range(N)]
                 axi = sns.lineplot(x, y, ax=ax[row,col], marker=".", markersize=markersize, hue=hue)
                 axi.axhline(0., color='red')
                 axi.set(ylabel=f'Error registered on dimension {i}',
                         xlabel=f'Traversal on dimension {dim}')
-                axi.set_xticklabels([f'{i:.2f}' for i in ticks])
+                #axi.set_xticklabels([f'{i:.2f}' for i in traversals_steps[dim].cpu().numpy()])
                 axi.tick_params(axis="x", labelsize=font_scale)
                 axi.tick_params(axis="y", labelsize=font_scale)
                 if ylim is not None: axi.set(ylim=(-ylim, ylim))
@@ -441,6 +439,30 @@ class ModelVisualiser(object):
         ax.set_xlabel(f'Latent dim {j}')
 
         return fig
+
+    @staticmethod
+    def scatterplot_with_line(x:Tensor, y:Tensor, hue:List, **kwargs):
+        """Plots the scatterplot and lineplot over the two input vectors x,y"""
+        figsize = kwargs.get("figsize",(20,10))
+        markersize = kwargs.get("markersize",10)
+        x_name = kwargs.get("x_name","First dim")
+        y_name = kwargs.get("y_name", "Second dim")
+        with_line = kwargs.get("with_line", False)
+        ylim = kwargs.get("ylim")
+        xlim = kwargs.get("xlim")
+
+        figure = plt.figure(figsize=figsize)
+        axi = sns.scatterplot(x, y,  marker=".", hue=hue, palette="viridis")
+        if with_line:
+            axi = sns.lineplot(x, y,  marker=".", markersize=markersize, hue=hue)
+        axi.set_title(f"Scatterplot of {x_name} and {y_name}")
+        axi.set_ylabel(f'Latent dim {y_name}')
+        axi.set_xlabel(f'Latent dim {x_name}')
+        if ylim is not None: axi.set(ylim=(-ylim, ylim))
+        if xlim is not None: axi.set(xlim=(-xlim, xlim))
+
+        return figure
+
 
 
     def plot_training_gradients(self, figsize=(12,12)):